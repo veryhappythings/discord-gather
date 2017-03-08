f8e6acd4a64aa4e07ddcc21c17e046a6be38b0bc
Mac Chapman <mac@veryhappythings.co.uk>
2017-03-08 11:50:51 +0000

    Add utf-8 codings

--- gather/commands.py
+++ gather/commands.py
@@ -1,3 +1,4 @@
+# coding: utf8
 from gather.organiser import NotEnoughPlayersError, PlayerNotFoundError
 
 
--- gather/gatherbot.py
+++ gather/gatherbot.py
@@ -1,3 +1,4 @@
+# coding: utf8
 import logging
 import discord
 from gather.bot import ListenerBot
